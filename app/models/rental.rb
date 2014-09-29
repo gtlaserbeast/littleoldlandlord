1728f0a2b007ff43b90bb9d7fd0afccdd60104c5
Will C. Fisher, Jr <willcfisherjr@gmail.com>
2014-09-28 20:02:12 -0400

    rental model, rentals controller, initial view

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,8 @@
 class Rental < ActiveRecord::Base
+  has_many :repairs
+  validates :address, presence: true
+  validates :address, uniqueness: true
+  validates :description, presence: true
+  geocoded_by :address
+  after_validation :geocode
 end
